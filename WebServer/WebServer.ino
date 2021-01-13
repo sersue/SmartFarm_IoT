d8ba189f50c70c9544f68b709e51f75515198c3d
MoonSueLym <msr56501354@gmail.com>
2021-01-13 23:41:06 +0900

    webserver

---
+++ WebServer/WebServer.ino
@@ -0,0 +1,153 @@
+/*
+ WiFiEsp example: WebServer
+
+ A simple web server that shows the value of the analog input 
+ pins via a web page using an ESP8266 module.
+ This sketch will print the IP address of your ESP8266 module (once connected)
+ to the Serial monitor. From there, you can open that address in a web browser
+ to display the web page.
+ The web page will be automatically refreshed each 20 seconds.
+
+ For more details see: http://yaab-arduino.blogspot.com/p/wifiesp.html
+*/
+
+#include "WiFiEsp.h"
+
+// Emulate Serial1 on pins 6/7 if not present
+#ifndef HAVE_HWSERIAL1
+#include "SoftwareSerial.h"
+SoftwareSerial Serial1(2, 3); // RX, TX
+#endif
+
+char ssid[] = "sersue";            // your network SSID (name)
+char pass[] = "anstnfla25%";        // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+int reqCount = 0;                // number of requests received
+int CDS =A1;
+int LED =5;
+WiFiEspServer server(80);
+
+
+void setup()
+{
+  // initialize serial for debugging
+  Serial.begin(9600);
+  // initialize serial for ESP module
+  Serial1.begin(9600);
+  // initialize ESP module
+  WiFi.init(&Serial1);
+
+  //조명 센서 
+  Serial.begin(9600);
+  pinMode(CDS,INPUT);
+  pinMode(LED,OUTPUT); 
+
+
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(ssid, pass);
+  }
+
+  Serial.println("You're connected to the network");
+  printWifiStatus();
+  
+  // start the web server on port 80
+  server.begin();
+}
+
+
+void loop()
+{
+  // listen for incoming clients
+  WiFiEspClient client = server.available();
+  if (client) {
+    Serial.println("New client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          Serial.println("Sending response");
+          
+          // send a standard http response header
+          // use \r\n instead of many println statements to speedup data send
+          client.print(
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n"  // the connection will be closed after completion of the response
+            "Refresh: 20\r\n"        // refresh the page automatically every 20 sec
+            "\r\n");
+          client.print("<!DOCTYPE HTML>\r\n");
+          client.print("<html>\r\n");
+          client.print("<h1> Hi </h1>\r\n");
+          CDS = analogRead(A1);
+
+          if(CDS>200){
+              digitalWrite(LED,HIGH); 
+              client.print("current light:");
+              client.print(CDS);
+              client.print("It's too dark, turnning lights");}
+          else{
+              digitalWrite(LED,LOW); 
+              client.print("current light:");
+              client.print(CDS);
+              client.print("It's too bright, turrning off lights");
+  }
+          delay(10000);
+  
+          client.print("<br>\r\n");
+          client.print("</html>\r\n");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(10);
+
+    // close the connection:
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+}
+
+
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  
+  // print where to go in the browser
+  Serial.println();
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
+  Serial.println();
+}
--- led_sensor/illuminance_sensor.ino/illuminance_sensor.ino.ino
+++ led_sensor/illuminance_sensor.ino/illuminance_sensor.ino.ino
@@ -6,21 +6,29 @@ void setup(){
   Serial.begin(9600);
   pinMode(CDS,INPUT);
   pinMode(LED,OUTPUT);
+  //excel
+  Serial.println("CLEARDATA");
+  Serial.println("Label,illuminance");
+
 }
 
 void loop(){
   CDS = analogRead(A1);
-  Serial.print("CDS_Sensor:");
+  Serial.print("C");
   Serial.println(CDS);
 
   if(CDS>200){
     digitalWrite(LED,HIGH);
-    Serial.println("LED ON");
+//    Serial.println("ON");
   }
   else{
     digitalWrite(LED,LOW);
-    Serial.println("LED OFF");
+//    Serial.println("OFF");
     
   }
-  delay(1000);
+  delay(10000);
+  Serial.end();
+
+  
+  
 }
